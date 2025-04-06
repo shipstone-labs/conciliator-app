73f803d9823686d391b57aec7ded8e0917a696bd
Andreas Richter <708186+richtera@users.noreply.github.com>
2025-04-06 13:53:44 -0400

    fix: Repair fixed top

--- components/HomeLink.tsx
+++ components/HomeLink.tsx
@@ -7,7 +7,7 @@ export default function HomeLink() {
     <span className="fixed top-6 left-6 w-12">
       <Link
         href="/"
-        className="fixed top-6 left-6 bg-muted w-12 h-12 flex items-center justify-center rounded-full shadow-xl hover:bg-muted/80 transition-all z-50 overflow-hidden border border-white/10"
+        className="bg-muted w-12 h-12 flex items-center justify-center rounded-full shadow-xl hover:bg-muted/80 transition-all z-50 overflow-hidden border border-white/10"
       >
         <Image
           src="/svg/Black+Yellow.svg"
