e8634b7b4008750680cf31a30dee16410ff79284
Cartwright Reed <cart@shipstone.com>
2025-04-08 14:21:11 -0400

    Revert "Update navigation header with logo, 'Explore Ideas' menu, and 'Sign In / Register' menu"
    This reverts commit 84e308fcc98a87b51fce5a6acd3199ca77bb59f3.

--- components/NavigationHeader.tsx
+++ components/NavigationHeader.tsx
@@ -3,46 +3,40 @@ import {
   MenubarContent,
   MenubarItem,
   MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function NavigationHeader() {
   return (
-    <div className="flex w-full items-center justify-between">
-      {/* Logo on left */}
-      <Link href="/" className="mr-4">
-        <Image
-          src="/svg/Black+Yellow.svg"
-          alt="SafeIdea Logo"
-          width={32}
-          height={32}
-          className="rounded-full"
-        />
-      </Link>
-      
-      {/* Main menu items */}
-      <Menubar className="border-none bg-transparent flex-grow">
-        <MenubarMenu>
-          <MenubarTrigger>Explore Ideas</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem>Browse Categories</MenubarItem>
-            <MenubarItem>Search</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
-      
-      {/* Right-aligned menu */}
-      <Menubar className="border-none bg-transparent">
-        <MenubarMenu>
-          <MenubarTrigger>Sign In / Register</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem>Sign In</MenubarItem>
-            <MenubarItem>Create Account</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
-    </div>
+    <Menubar className="border-none bg-transparent">
+      <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New <MenubarShortcut>⌘N</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>Open</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Save</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Edit</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>
+            Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+          </MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Cut</MenubarItem>
+          <MenubarItem>Copy</MenubarItem>
+          <MenubarItem>Paste</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
   );
 }
