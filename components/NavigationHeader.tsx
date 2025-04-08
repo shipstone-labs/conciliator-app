1914e81385547ff5ae944a40d028c966058363a6
Cartwright Reed <cart@shipstone.com>
2025-04-08 16:33:08 -0400

    Remove unused menubar imports to fix lint errors

--- components/NavigationHeader.tsx
+++ components/NavigationHeader.tsx
@@ -1,10 +1,6 @@
 import {
   Menubar,
-  MenubarContent,
-  MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import Image from "next/image";
