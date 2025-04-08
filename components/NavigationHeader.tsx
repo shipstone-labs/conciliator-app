3c72a53d00024a9006b3a03bb623f18be7f025b5
Cartwright Reed <cart@shipstone.com>
2025-04-08 15:05:33 -0400

    Update navigation header with logo and 'Sign In / Register' button

--- components/NavigationHeader.tsx
+++ components/NavigationHeader.tsx
@@ -7,36 +7,61 @@ import {
   MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
+import Image from "next/image";
+import Link from "next/link";
+import { AuthButton } from "./AuthButton";
 
 export default function NavigationHeader() {
   return (
-    <Menubar className="border-none bg-transparent">
-      <MenubarMenu>
-        <MenubarTrigger>File</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            New <MenubarShortcut>⌘N</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>Open</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Save</MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>Edit</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            Undo <MenubarShortcut>⌘Z</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>
-            Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Cut</MenubarItem>
-          <MenubarItem>Copy</MenubarItem>
-          <MenubarItem>Paste</MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-    </Menubar>
+    <div className="flex w-full items-center justify-between">
+      {/* Logo on left */}
+      <Link href="/" className="mr-4">
+        <Image
+          src="/svg/Black+Yellow.svg"
+          alt="SafeIdea Logo"
+          width={32}
+          height={32}
+          className="rounded-full"
+        />
+      </Link>
+      
+      {/* Main menu items */}
+      <Menubar className="border-none bg-transparent flex-grow">
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New <MenubarShortcut>⌘N</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>Open</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Save</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>Edit</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>
+              Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+            </MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Cut</MenubarItem>
+            <MenubarItem>Copy</MenubarItem>
+            <MenubarItem>Paste</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+      
+      {/* Right-aligned Sign In button */}
+      <div className="ml-4">
+        <AuthButton 
+          text="Sign In / Register" 
+          className="bg-primary hover:bg-primary/80 text-black font-medium rounded-md shadow-md" 
+        />
+      </div>
+    </div>
   );
 }
