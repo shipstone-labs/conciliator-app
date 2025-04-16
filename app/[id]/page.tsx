c5ec291ab47aaa2131fb32b4b3c06fdfadca773b
Andreas Richter <708186+richtera@users.noreply.github.com>
2025-04-16 14:34:36 -0400

    fix: Cleanup unused newDocId

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -7,9 +7,5 @@ export default function Token() {
   const params = useParams()
   const docId = params?.id as string // Retrieve the tokenId from the dynamic route
 
-  const handleCreateNewIP = () => {
-    // Redirect to the root page
-    window.location.href = '/'
-  }
-  return <QuestionIP docId={docId} onNewIP={handleCreateNewIP} />
+  return <QuestionIP docId={docId} />
 }
