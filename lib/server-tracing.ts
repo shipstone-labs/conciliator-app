56d8b1bb425298f0a9c05a32c4393a86c17e3cbe
Andreas Richter <708186+richtera@users.noreply.github.com>
2025-04-26 14:38:52 -0400

    fix: Damn service name still not working

--- lib/server-tracing.ts
+++ lib/server-tracing.ts
@@ -81,7 +81,9 @@ export async function initServerTracing() {
   // Then in your initialization code:
   // Choose the appropriate exporter based on environment
   const exporter = isGCP
-    ? new TraceExporter()
+    ? new TraceExporter({
+        resourceFilter: /^(service\.)/,
+      })
     : new OTLPTraceExporter({
         url:
           process.env.OTEL_EXPORTER_OTLP_ENDPOINT ||
