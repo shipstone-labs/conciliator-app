e7e7e6b7d905838d273f32084cbb3cd2fca5795c
Andreas Richter <708186+richtera@users.noreply.github.com>
2025-05-09 22:29:24 -0400

    fix: WIP

---
+++ hooks/useFeature.tsx
@@ -0,0 +1,34 @@
+import { useConfig } from '@/components/AuthLayout'
+
+export type Feature =
+  | 'stripe'
+  | 'stytch'
+  | 'lit'
+  | 'lilypad'
+  | 'openai'
+  | 'storacha'
+  | 'firestore'
+  | 'firebase'
+  | 'bucket'
+
+export function useFeatures(): Partial<Record<Feature, boolean>> {
+  const config = useConfig()
+  // This is a placeholder implementation.
+  // In a real application, you would replace this with logic to check if the feature is enabled.
+  const enabledFeatures = ((config.FEATURES as string) || '')
+    .split(',')
+    .filter(Boolean) as Feature[] // Example list of enabled features
+  return Object.fromEntries(
+    enabledFeatures.map((feature) => [feature, true] as [Feature, boolean])
+  )
+}
+
+export function useFeature(
+  featureName: string
+): boolean | Record<string, boolean> {
+  const enabledFeatures = useFeatures()
+  return (
+    (featureName in enabledFeatures && (enabledFeatures as any)[featureName]) ??
+    false
+  )
+}
