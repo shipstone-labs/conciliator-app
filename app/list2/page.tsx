4e3b3e1eeb13a2251cbe3f2812b11fa8f2a2684a
Cartwright Reed <cart@shipstone.com>
2025-05-14 20:27:19 -0400

    feat: Add basic /list2 route structure for new IP list view

---
+++ app/list2/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function List2Page() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">New IP List View (Testing)</h1>
+      <p>This is a test implementation of the new list view.</p>
+    </div>
+  )
+}
